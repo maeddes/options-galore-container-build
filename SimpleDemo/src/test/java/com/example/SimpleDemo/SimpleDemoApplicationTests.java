75c3143b3f8f4cdc23d0e63b3f0a773a028701f2
Matthias Haeussler <maeddes@gmail.com>
2021-04-23 12:21:18 +0200

    Upped Spring Boot Version

---
+++ SimpleDemo/src/test/java/com/example/SimpleDemo/SimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
